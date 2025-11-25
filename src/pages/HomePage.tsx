fe4942a43519ccc4db5ed573e12559f72b49d896
reyhkm <reyhikam04@gmail.com>
2025-11-25 16:48:23 +0700

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,23 @@
-import { Container, Title, Paper, Text, Anchor } from '@mantine/core';
-import { Link } from 'react-router-dom';
+import { Container, Title, Paper, Text, Stack } from '@mantine/core';
 import ReportForm from '../components/forms/ReportForm';
 
 function HomePage() {
   return (
     <Container size="sm" my="xl">
-      <Title ta="center">SILAWA</Title>
-      <Text c="dimmed" size="sm" ta="center" mt={5} mb={30}>
-        Sistem Layanan Pengaduan Warga
-      </Text>
-
-      <Paper withBorder shadow="md" p={30} radius="md">
-        <Title order={2} mb="md">Buat Laporan Baru</Title>
-        <ReportForm />
-      </Paper>
-      
-      <Text ta="center" mt="lg">
-        Sudah punya laporan? <Anchor component={Link} to="/lacak">Lacak di sini</Anchor>.
-      </Text>
+      <Stack align="center" gap="lg">
+        <Title order={1} ta="center">
+          Layanan Pengaduan Warga Online
+        </Title>
+        <Text c="dimmed" size="lg" ta="center" maw={600}>
+          Sampaikan laporan Anda langsung kepada kami. Setiap laporan akan kami proses untuk pelayanan publik yang lebih baik.
+        </Text>
+        <Paper p="xl" withBorder style={{ width: '100%' }}>
+          <Title order={3} mb="md">
+            Formulir Laporan Baru
+          </Title>
+          <ReportForm />
+        </Paper>
+      </Stack>
     </Container>
   );
 }
